9e298b437522517afedd7bb991e84aadfd058805
steduq10 <steven.duque.95@gmail.com>
2022-05-10 13:09:09 -0500

    Se configura el código para intercambiar los elelementos del diccionario

--- MensajeEncriptado.py
+++ MensajeEncriptado.py
@@ -1,13 +1,52 @@
 mensaje = input("Ingrese el mensaje: ")
-ABC = ["A","B","C","D","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"]
+#ABC = ["A","B","C","D","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"]
+ABC = ("ABCDEFGHIJKLMNOPQRSTUVWXYZ")
 listaMensaje = []
 for i in mensaje:
     listaMensaje.append(i)
 print(listaMensaje)
 
+diccionario = {}
+print("('",end="")
+#i = 0
+#while i <= len(listaMensaje):
+   # print(ABC[i],end="")
+   # i += 1
+
+
+#print("' ,")
+
 for i in range(len(listaMensaje)):
-    diccionario = {ABC[i]:listaMensaje[i]}
-    print(diccionario)
+    print(ABC[i], end="")
+print("' ,")
+
+
+for i in range(len(listaMensaje)):
+
+    diccionario[listaMensaje[i]] = ABC[i]
+    diccionario.update({listaMensaje[i]:ABC[i]})
+
+print(diccionario,")")
+#i = 0
+#while i <= len(listaMensaje):
+    #diccionario[listaMensaje[i]] = ABC[i]
+    #print(diccionario)
+    #i += 1
+
+#print(")")
+
+##DEBO INVERTIR LAS LLAVES CON LOS VALORES
+print(diccionario.values())
+print(diccionario.keys())
+
+diccionarioInvertido = {} #SE CREA UN NUEVO DICCIONARIO VACIO QUE ALMACENARÁ EL NUEVO DICCIONARIO INVERTIDO
+for key, value in diccionario.items(): #SE CREA UN CICLO FOR PARA INTERCAMBIAR LOS ELEMENTOS DEL DICCIONARIO
+    diccionarioInvertido[value] = key
+
+print(diccionarioInvertido)
+print(diccionarioInvertido.values())
+print(diccionarioInvertido.keys())
+
 
 #ABC = {"A":listaMensaje[0],"B":listaMensaje[1],"C":listaMensaje[2],"D":listaMensaje[3],
     #   "E":listaMensaje[4],"F":listaMensaje[5],"G":listaMensaje[6],"H":listaMensaje[7],
