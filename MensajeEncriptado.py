347ac46ac12f157a4b80464479c27e3d22d1388d
steduq10 <steven.duque.95@gmail.com>
2022-05-10 19:01:56 -0500

    Se corrige la lógica del código. Se configura el código para que reescriba el mensaje original con el código de encriptado

--- MensajeEncriptado.py
+++ MensajeEncriptado.py
@@ -2,31 +2,54 @@ mensaje = input("Ingrese el mensaje: ")
 #ABC = ["A","B","C","D","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"]
 ABC = ("ABCDEFGHIJKLMNOPQRSTUVWXYZ")
 listaMensaje = []
+
 for i in mensaje:
     listaMensaje.append(i)
 print(listaMensaje)
 
 diccionario = {}
 print("('",end="")
-#i = 0
-#while i <= len(listaMensaje):
-   # print(ABC[i],end="")
-   # i += 1
+
 
 
 #print("' ,")
 
-for i in range(len(listaMensaje)):
-    print(ABC[i], end="")
-print("' ,")
 
 
+print("Se imprime el diccionario")
 for i in range(len(listaMensaje)):
+    #diccionario[ABC[i]] = listaMensaje[i]
+    #diccionario.update({ABC[i]: listaMensaje[i]})
 
     diccionario[listaMensaje[i]] = ABC[i]
-    diccionario.update({listaMensaje[i]:ABC[i]})
+    #diccionario.update({listaMensaje[i]:ABC[i]})
+
 
 print(diccionario,")")
+print("CLAVE")
+valor = diccionario.values()
+#clave = diccionario.keys()
+
+lista =list(diccionario)
+print(lista)
+print("FOR")
+'''
+for i in diccionario:
+    #print(i)
+    print(diccionario[i])
+'''
+#Creamos un ciclo for para reescribir el mensaje original por el código de encriptación, Para ello hacemos un
+#ciclo anidado en el cual tenemos: un for externo para que itere dentro del diccionario y uno interno para que
+#itere en el rango del tamaño del mensaje ingresado con el condicional de que si la clave del diccionario
+#coincide con el caracter del mensaje, coga el valor del diccionario y se lo asigne a la lista.
+for i in diccionario:
+    for j in range(len(listaMensaje)):
+        if i == listaMensaje[j]:
+            listaMensaje[j] = diccionario[i]
+
+
+print(listaMensaje)
+
 #i = 0
 #while i <= len(listaMensaje):
     #diccionario[listaMensaje[i]] = ABC[i]
@@ -35,10 +58,10 @@ print(diccionario,")")
 
 #print(")")
 
-##DEBO INVERTIR LAS LLAVES CON LOS VALORES
-print(diccionario.values())
-print(diccionario.keys())
+#DECODIFICADOR
 
+##DEBO INVERTIR LAS LLAVES CON LOS VALORES
+print("Se imprime el diccionario invertido")
 diccionarioInvertido = {} #SE CREA UN NUEVO DICCIONARIO VACIO QUE ALMACENARÁ EL NUEVO DICCIONARIO INVERTIDO
 for key, value in diccionario.items(): #SE CREA UN CICLO FOR PARA INTERCAMBIAR LOS ELEMENTOS DEL DICCIONARIO
     diccionarioInvertido[value] = key
@@ -47,22 +70,17 @@ print(diccionarioInvertido)
 print(diccionarioInvertido.values())
 print(diccionarioInvertido.keys())
 
+for i in diccionarioInvertido.values():
+    print(i,end="")
 
-#ABC = {"A":listaMensaje[0],"B":listaMensaje[1],"C":listaMensaje[2],"D":listaMensaje[3],
-    #   "E":listaMensaje[4],"F":listaMensaje[5],"G":listaMensaje[6],"H":listaMensaje[7],
-   #    "I":listaMensaje[8],"J":listaMensaje[9],"K":listaMensaje[10],"L":listaMensaje[11],
-   #    "M":listaMensaje[12],"N":listaMensaje[13],"O":listaMensaje[14],"P":listaMensaje[15],
-   #    "Q":listaMensaje[16],"R":listaMensaje[17],"S":listaMensaje[18],"T":listaMensaje[19],
-     #  "U":listaMensaje[20],"V":listaMensaje[21],"W":listaMensaje[22],"X":listaMensaje[23],
-  #     "Y":listaMensaje[24],"Z":listaMensaje[25]}
 
-#print(ABC)
-#for i in mensaje :
-    #print(i)
-   # listaMensaje[i]
-    #ABC[i]
 
-    #diccionario[ABC] = listaMensaje
-#print(diccionario)
+
+
+
+
+
+
+
 
 
