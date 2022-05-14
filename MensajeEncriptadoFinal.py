e7d7fb6fb612c83c114a580bb8209bd2ab287fe7
steduq10 <steven.duque.95@gmail.com>
2022-05-14 18:21:57 -0500

    Se carga la version final

--- MensajeEncriptadoFinal.py
+++ MensajeEncriptadoFinal.py
@@ -24,7 +24,15 @@ def encriptador(mensaje):
     for i in listaMensaje:
         encriptado += clave[i]
 
+    #print(f"Con {mensaje} como mensaje, se obtuvo el siguiente mensaje encriptado, ")
+    #print(" ")
+    #print("clave y mensaje desencriptado respectivamente:",end=" ")
+    print(encriptado+",",clave,end=", ")
+    #print("('" + encriptado, end="',")
+    #print(clave, end="")
+    #print(",",end=" ")
 
+    #print("Con",mensaje,"como mensaje, se obtuvo el siguiente mensaje encriptado,\n"+"clave y mensaje desencriptado respectivamente:", encriptado+",", clave,",",end=" ")
     return encriptado,clave
 
 def desencriptador(encriptado, clave):
@@ -36,16 +44,17 @@ def desencriptador(encriptado, clave):
     desencriptado = ""
     for i in encriptado:
         desencriptado += diccionarioInvertido[i]#diccionarioInvertido[i]
+    print(desencriptado)
     return desencriptado
 
 mensaje = input("Ingrese el mensaje: ")
-encriptador(mensaje)
+#encriptador(mensaje)
 
 encriptado, clave = encriptador(mensaje)
 desencriptador(encriptado,clave)
-print("(",encriptado,end=",")
-print(clave,end="")
-print(")")
-desencriptado = desencriptador(encriptado,clave)
-print(desencriptado)
+#print("('"+encriptado,end="',")
+#print(clave,end="")
+#print(")")
+#desencriptado = desencriptador(encriptado,clave)
+#print(desencriptado)
 
